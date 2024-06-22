f96f1fe2226f209ef1fce102ba390b087ab17033
Walid Siala <wsiala4@gmail.com>
2024-06-22 17:27:11 +0100

    Added Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,20 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
+  const t = useTranslations('Navbar');
+  const linkStyle = "font-medium hover:text-my-blue duration-300 transition-all hover:bg-slate-50 h-fit py-2 px-4"
+
   return (
    <>
-    <div className="py-10">
+    <div className="py-10 flex justify-between">
         <Image src="/logo.svg" alt="logo" width={200} height={100} />
+        <div className="flex">
+            <Link className={linkStyle} href="/">{t("home")}</Link>
+            <Link className={linkStyle} href="/">{t("about")}</Link>
+            <Link className={linkStyle} href="/">{t("contact")}</Link>
+        </div>
     </div>
    </>
   )
